bd444847c26c6e4b120cb23a6cbba63056d283e6
Ken <mulilamwendwa@gmail.com>
2021-06-21 21:59:26 +0300

    Admin dashboard

---
+++ resources/views/layouts/footerMenu.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+     version 1.0.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2021 <a href="#">Mulila</a>.</strong> All rights reserved.
+  </footer>
---
+++ resources/views/layouts/rightMenu.blade.php
@@ -0,0 +1,9 @@
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <button>
+        <h5 class="background-color:green">Log Out</h5>
+      </button>
+    </div>
+  </aside>
+ 
